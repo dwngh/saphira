2956747ed986bcc4343b8d6a0470ee48423ae813
Phuc Long Nguyen <20021387@vnu.edu.vn>
2022-10-11 18:33:17 +0700

    Init API
    - init API
    - need to be reviewed
    - forget my DB_password pls :D

---
+++ APIforSaphira/server.ts
@@ -0,0 +1,26 @@
+import appRouter from "./src/route/routes";
+import express, {application} from "express";
+import cors from 'cors';
+import helmet from 'helmet'
+import * as dotenv from 'dotenv'
+
+export default class App {
+    private app;
+    constructor() {
+        this.app = express();
+
+        dotenv.config();
+        this.app.set('port', process.env.PORT);
+
+        this.app.use(cors());
+        this.app.use(helmet());
+        this.app.use(express.json());
+
+        this.app.use(appRouter);
+    }
+
+    async listen() {
+        this.app.listen(3333);
+        console.log("Server started at localhost:3333");
+    }
+}
---
+++ APIforSaphira/src/index.ts
@@ -0,0 +1,8 @@
+import App from "../server";
+
+async function main() {
+    const app = new App();
+    app.listen();
+}
+
+main();
