a8c3e3ede7d26365f25abd6ed9ff863b544e0bc1
dothanhdat0907 <75473674+dothanhdat0907@users.noreply.github.com>
2022-12-08 23:53:32 +0700

    - change eng to vn

--- front_end/common/time.tsx
+++ front_end/common/time.tsx
@@ -4,15 +4,18 @@ interface DayAvail {
 }
 
 export const TimeSolve = () => {
-    const allDayofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    const allDayofWeek = ['Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy', 'Chủ Nhật']
     const getAvailableDay = (avail:string) => {  
         let day_avail:DayAvail = { 
             shiftStr: avail, 
             dayStr:[]
         }
-        for(var i = 0; i < 7; i++) {
+        for(let i = 0; i < 7; i++) {
             let day = avail.slice(i*8, i*8+8)
-            if(day.includes('0')) day_avail.dayStr.push(allDayofWeek[i])
+            if(day.includes('1')) day_avail.dayStr.push(allDayofWeek[i])
+        }
+        if (day_avail.dayStr.length === 7) {
+            day_avail.dayStr = ['Cả tuần']
         }
         return day_avail
     }
@@ -26,10 +29,10 @@ export const TimeSolve = () => {
     }
 
     const getCurrentShift = (order_arr:any) => {
-        let allOrderinDay = [0, 0, 0, 0, 0, 0, 0]
-        for(var i = 0; i < order_arr.length; i++) {
+        let allOrderinDay = [0, 0, 0, 0, 0, 0, 0, 0]
+        for(let i = 0; i < order_arr.length; i++) {
             let shift = order_arr[i]
-            for(var j = 0; j < shift.length; j++) {
+            for(let j = 0; j < shift.length; j++) {
                 if(shift[j] === '1') allOrderinDay[j]+=1
             }
         }
